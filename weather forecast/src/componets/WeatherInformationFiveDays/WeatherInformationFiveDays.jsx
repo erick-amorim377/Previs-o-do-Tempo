fefcbe4bd8f55bf53154c73863ff60c47ca5afc5
erick-amorim377 <erickamorim221@gmail.com>
2025-03-19 12:30:26 -0300

    finalização da previsão dos 5 proximos dias

--- weather forecast/src/componets/WeatherInformationFiveDays/WeatherInformationFiveDays.jsx
+++ weather forecast/src/componets/WeatherInformationFiveDays/WeatherInformationFiveDays.jsx
@@ -14,11 +14,33 @@ function WeatherInformationFiveDays({fiveWeather}) {
     
   }
 
-console.log(dailyForecast)
+  const nextFiveDays = Object.values(dailyForecast).slice(1,6);
+
+  function convertDay(date){
+  
+    const newDate = new Date(date.dt * 1000).toLocaleDateString('pt-Br', {weekday: 'long', day: "2-digit"})
+
+    return newDate
+  }
+
 
   return (
     <div className='weatherContainer'>
-      <p>five days</p>
+
+      <h3>Previsão dos Proximos 5 dias</h3>
+
+      <div className='weatherList'>
+
+      {nextFiveDays.map(forecast => (
+        <div key={forecast.td} className='weatherItem'>
+          <p>{convertDay(forecast)}</p>
+          <img src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}/>
+          <p>{forecast.weather[0].description}</p>
+          <p>{Math.round(forecast.main.temp_min)}°C min / {Math.round(forecast.main.temp_max)}°C max</p>
+        </div>
+      ))}
+      </div>
+
     </div>
   )
 }
